3648b4aa0d3735c24ef62f06c24b8eea473054ac
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-08-29 15:12:43 +0900

    feat : set project
    프로젝트 초기 설정 완료

---
+++ src/main/java/com/wonderplace/WonderPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.wonderplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wonderplace/WonderPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonderplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
