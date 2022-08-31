20acdc54b7ef978c58ec62e1e9be971692cfa9c7
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-08-31 17:41:05 +0900

    feat : add join and login
    회원가입과 로그인을 구현하였다.
    회원가입시 db에 패스워드는 암호화 되어 저장된다.
    로그인시 jwt 토큰을 반환해준다. 추후 인증에 사용할 예정이다.

--- src/main/java/com/wonderplace/controller/api/UserApiController.java
+++ src/main/java/com/wonderplace/controller/api/UserApiController.java
@@ -1,8 +1,15 @@
 package com.wonderplace.controller.api;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wonderplace.controller.request.UserJoinRequest;
+import com.wonderplace.controller.request.UserLoginRequest;
+import com.wonderplace.controller.response.Response;
+import com.wonderplace.controller.response.UserJoinResponse;
+import com.wonderplace.controller.response.UserLoginResponse;
+import com.wonderplace.model.User;
 import com.wonderplace.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,7 +21,15 @@ public class UserApiController {
 	private final UserService userService;
 
 	@PostMapping("/join")
-	public void join() {
-		userService.join("", "");
+	public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
+		User user = userService.join(request.getUsername(), request.getPassword());
+		return Response.success(UserJoinResponse.from(user));
 	}
+
+	@PostMapping("/login")
+	public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request) {
+		String token = userService.login(request.getUsername(), request.getPassword());
+		return Response.success(UserLoginResponse.of(token));
+	}
+
 }
--- src/main/java/com/wonderplace/service/UserService.java
+++ src/main/java/com/wonderplace/service/UserService.java
@@ -1,39 +1,57 @@
 package com.wonderplace.service;
 
-import java.util.Optional;
+import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.wonderplace.exception.ErrorCode;
 import com.wonderplace.exception.WonderPlaceException;
 import com.wonderplace.model.User;
 import com.wonderplace.model.entity.UserEntity;
 import com.wonderplace.repository.UserEntityRepository;
+import com.wonderplace.util.JwtTokenUtils;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
 	private final UserEntityRepository userEntityRepository;
+	private final BCryptPasswordEncoder encoder;
 
+	@Value("${jwt.secret-key}")
+	private String secretKey;
+
+	@Value("${jwt.token.expired-time-ms}")
+	private Long expiredTimeMS;
+
+	@Transactional
 	public User join(String username, String password) {
-		Optional<UserEntity> userEntity = userEntityRepository.findByUsername(username);
+		userEntityRepository.findByUsername(username).ifPresent(it -> {
+			throw new WonderPlaceException(ErrorCode.DUPLICATED_USER_NAME,
+				String.format("\"%s\" is duplicated", username));
+		});
 
-		userEntityRepository.save(new UserEntity());
-
-		return new User();
+		UserEntity userEntity = userEntityRepository.save(UserEntity.of(username, encoder.encode(password)));
+		log.info("Success User Join - \"{}\"", username);
+		return User.from(userEntity);
 	}
 
 	public String login(String username, String password) {
-
 		UserEntity userEntity = userEntityRepository.findByUsername(username)
-			.orElseThrow(() -> new WonderPlaceException());
+			.orElseThrow(
+				() -> new WonderPlaceException(ErrorCode.USER_NOT_FOUND,
+					String.format("\"%s\" not founded", username)));
 
-		userEntity.checkPassword(password);
+		if (!encoder.matches(password, userEntity.getPassword())) {
+			throw new WonderPlaceException(ErrorCode.INVALID_PASSWORD);
+		}
 
-		//토큰생성
-
-		return "";
+		return JwtTokenUtils.generateToken(username, secretKey, expiredTimeMS);
 	}
 }
