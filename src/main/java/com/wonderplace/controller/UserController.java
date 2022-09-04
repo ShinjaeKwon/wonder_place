bf939f1252b0cea3e488cca898f5ca318a5662ec
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-09-04 15:13:43 +0900

    rename : controller 이름 변경

--- src/main/java/com/wonderplace/controller/UserController.java
+++ src/main/java/com/wonderplace/controller/UserController.java
@@ -1,7 +1,35 @@
 package com.wonderplace.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.wonderplace.controller.request.UserJoinRequest;
+import com.wonderplace.controller.request.UserLoginRequest;
+import com.wonderplace.controller.response.Response;
+import com.wonderplace.controller.response.UserJoinResponse;
+import com.wonderplace.controller.response.UserLoginResponse;
+import com.wonderplace.model.User;
+import com.wonderplace.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
 public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping("/join")
+	public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
+		User user = userService.join(request.getUsername(), request.getPassword());
+		return Response.success(UserJoinResponse.from(user));
+	}
+
+	@PostMapping("/login")
+	public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request) {
+		String token = userService.login(request.getUsername(), request.getPassword());
+		return Response.success(UserLoginResponse.of(token));
+	}
+
 }
